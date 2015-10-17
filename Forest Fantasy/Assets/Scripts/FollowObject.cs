8e84ae84051714cc7845d52d08d710ec34778f00
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-10-17 22:39:44 +0300

    The player moves by clicking and the camera follows it

---
+++ Forest Fantasy/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+	public Transform followee;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = followee.position + offset;
+	}
+}
